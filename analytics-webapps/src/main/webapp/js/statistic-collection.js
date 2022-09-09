da87ae8410f2a7df66530974285eb6cfcb5bdc89
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-09-09 10:14:08 +0100

    MEED-464: Add Analytics for space topbar popover (#70)
    In this PR we add analytics the use of space topbar popover actions:
    
    Display space hosts list
    Bookmark/ unbookmark space
    Back to home action

--- analytics-webapps/src/main/webapp/js/statistic-collection.js
+++ analytics-webapps/src/main/webapp/js/statistic-collection.js
@@ -60,6 +60,7 @@ function() {
         document.addEventListener('search-tag', () => this.addStatisticSearchByTag());
         document.addEventListener('editors-options-opened', event => this.addStatisticEditorOptionsOpened(event && event.detail));
         document.addEventListener('editor-option-added', event => this.addStatisticEditorOptionAdded(event && event.detail));
+        document.addEventListener('space-topbar-popover-action', event => this.addStatisticSpaceTopbarPopover(event && event.detail));
         document.addEventListener('search-favorites-selected', () => this.sendMessage({
           'module': 'portal',
           'subModule': 'ui',
@@ -112,6 +113,21 @@ function() {
       };
       this.sendMessage(editorOptionAddedAnalytics);
     },
+    addStatisticSpaceTopbarPopover: function (eventDetail) {
+      const spaceTopbarPopoverAnalytics = {
+        'module': 'portal',
+        'subModule': 'ui',
+        'userId': eXo.env.portal.userIdentityId,
+        'userName': eXo.env.portal.userName,
+        'operation': eventDetail,
+        'name': 'spaceTopbarpopover',
+        'timestamp': Date.now(),
+        'parameters': {
+          'entityType': 'SPACE_TOP_BAR_TIPTIP',
+        },
+      };
+      this.sendMessage(spaceTopbarPopoverAnalytics);
+    },
     addStatisticStreamFilter: function (streamFilter) {
       const streamFilterAnalytics = {
         'module': 'portal',
@@ -126,6 +142,7 @@ function() {
     },
     addStatisticFavorite: function (bookmark, eventDetail) {
       let type = eventDetail.typeLabel || eventDetail.type;
+      let entityType = eventDetail.entityType || '';
       this.sendMessage({
         'module': 'portal',
         'subModule': 'ui',
@@ -139,6 +156,7 @@ function() {
           'dataType': type.toLowerCase(),
           'contentId': eventDetail.id,
           'spaceId': eventDetail.spaceId,
+          'entityType': entityType,
         },
       });
     },
