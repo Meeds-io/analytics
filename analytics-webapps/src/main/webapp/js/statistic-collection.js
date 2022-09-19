5ff21fddd44084da06b68176c6653a0106acd65a
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-09-19 17:38:28 +0100

    feat: left menu review Operations for left menu action - MEED-549 - Meeds-io/meeds#16 (#76)
    Add Analytics to track actions in the spaces left navigation menu.

--- analytics-webapps/src/main/webapp/js/statistic-collection.js
+++ analytics-webapps/src/main/webapp/js/statistic-collection.js
@@ -61,6 +61,7 @@ function() {
         document.addEventListener('editors-options-opened', event => this.addStatisticEditorOptionsOpened(event && event.detail));
         document.addEventListener('editor-option-added', event => this.addStatisticEditorOptionAdded(event && event.detail));
         document.addEventListener('space-topbar-popover-action', event => this.addStatisticSpaceTopbarPopover(event && event.detail));
+        document.addEventListener('space-left-navigation-action', event => this.addStatisticSpaceLeftNavigation(event && event.detail));
         document.addEventListener('search-favorites-selected', () => this.sendMessage({
           'module': 'portal',
           'subModule': 'ui',
@@ -127,6 +128,21 @@ function() {
         },
       };
       this.sendMessage(spaceTopbarPopoverAnalytics);
+    },
+    addStatisticSpaceLeftNavigation: function (eventDetail) {
+      const spaceLeftNavigationAnalytics = {
+        'module': 'portal',
+        'subModule': 'ui',
+        'userId': eXo.env.portal.userIdentityId,
+        'userName': eXo.env.portal.userName,
+        'operation': eventDetail,
+        'name': 'spaceLeftNavigation',
+        'timestamp': Date.now(),
+        'parameters': {
+          'entityType': 'spaces_left_navigation',
+        },
+      };
+      this.sendMessage(spaceLeftNavigationAnalytics);
     },
     addStatisticStreamFilter: function (streamFilter) {
       const streamFilterAnalytics = {
