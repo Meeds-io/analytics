69fad0fbd0744839a40982bff98d08615c681d1c
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-09-26 14:40:41 +0100

    feat: Add Analytics for attach images - MEED-2435 - Meeds-io/MIPS#53 (#135)
    This PR allows to adds analytics for attach image.

---
+++ analytics-listeners/src/main/java/org/exoplatform/analytics/listener/social/AnalyticsAttachmentListener.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.exoplatform.analytics.listener.social;
+
+import org.exoplatform.analytics.model.StatisticData;
+import org.exoplatform.analytics.utils.AnalyticsUtils;
+import org.exoplatform.commons.api.persistence.ExoTransactional;
+import org.exoplatform.services.listener.Asynchronous;
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+import org.exoplatform.social.attachment.AttachmentPlugin;
+import org.exoplatform.social.attachment.AttachmentService;
+import org.exoplatform.social.attachment.model.ObjectAttachmentId;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
+
+import java.util.Date;
+import java.util.Map;
+
+import static org.exoplatform.analytics.utils.AnalyticsUtils.addSpaceStatistics;
+import static org.exoplatform.analytics.utils.AnalyticsUtils.addStatisticData;
+
+@Asynchronous
+public class AnalyticsAttachmentListener extends Listener<String, ObjectAttachmentId> {
+  public static final String      STATISTICS_ATTACH_OPERATION = "attachImages";
+
+  public static final String      STATISTICS_DETACH_OPERATION = "removeImageAttachments";
+
+  public static final String      ATTACHMENT_CREATED_EVENT    = "attachment.created";
+
+  public static final String      ATTACHMENT_DELETED_EVENT    = "attachment.deleted";
+
+  private final AttachmentService attachmentService;
+
+  private SpaceService            spaceService;
+
+  public AnalyticsAttachmentListener(AttachmentService attachmentService, SpaceService spaceService) {
+    this.attachmentService = attachmentService;
+    this.spaceService = spaceService;
+  }
+
+  @Override
+  @ExoTransactional
+  public void onEvent(Event<String, ObjectAttachmentId> event) throws Exception {
+
+    String username = event.getSource();
+    ObjectAttachmentId objectAttachment = event.getData();
+
+    if (objectAttachment == null) {
+      return;
+    }
+
+    Map<String, AttachmentPlugin> attachmentPlugins = attachmentService.getAttachmentPlugins();
+    AttachmentPlugin attachmentPlugin = attachmentPlugins.get(objectAttachment.getObjectType());
+    long spaceId = attachmentPlugin.getSpaceId(objectAttachment.getObjectId());
+
+    long userId = AnalyticsUtils.getUserIdentityId(username);
+
+    switch (event.getEventName()) {
+    case ATTACHMENT_CREATED_EVENT: {
+      buildStatisticData(STATISTICS_ATTACH_OPERATION, spaceId, userId);
+      break;
+    }
+    case ATTACHMENT_DELETED_EVENT: {
+      buildStatisticData(STATISTICS_DETACH_OPERATION, spaceId, userId);
+      break;
+    }
+    default:
+      throw new IllegalArgumentException("Unexpected listener event name: " + event.getEventName());
+    }
+  }
+
+  private void buildStatisticData(String operation, long spaceId, long userId) {
+    Space space = spaceService.getSpaceById(String.valueOf(spaceId));
+    StatisticData statisticData = new StatisticData();
+    statisticData.setModule("social");
+    statisticData.setSubModule("attachment");
+    statisticData.setOperation(operation);
+    statisticData.setTimestamp(new Date().getTime());
+    statisticData.setUserId(userId);
+    addSpaceStatistics(statisticData, space);
+    addStatisticData(statisticData);
+  }
+}
