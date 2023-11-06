c2edd82b2489a5c5115a4ea0223f5d7ce8b99479
saraboutej <sboutej@exoplatform.com>
2023-11-06 12:07:09 +0100

    feat: No access to items that are overflow - MEED-2728-Meeds-io/MIPs#81

--- analytics-webapps/src/main/webapp/vue-app/rate-portlet/components/AnalyticsRateApplication.vue
+++ analytics-webapps/src/main/webapp/vue-app/rate-portlet/components/AnalyticsRateApplication.vue
@@ -17,7 +17,7 @@
 <template>
   <v-app 
     :id="appId"
-    class="analytics-application card-border-radius overflow-hidden"
+    class="analytics-application card-border-radius"
     flat>
     <template v-if="canEdit">
       <analytics-chart-setting
@@ -27,7 +27,7 @@
         class="mt-0"
         @save="saveSettings" />
     </template>
-    <v-card class="d-flex flex-column ma-auto analytics-chart-percentage analytics-chart-parent white" flat>
+    <v-card class="d-flex flex-column ma-auto analytics-chart-percentage analytics-chart-parent transparent" flat>
       <div class="d-flex pa-3 analytics-chart-header" flat>
         <v-toolbar-title class="d-flex">
           <v-tooltip bottom>
