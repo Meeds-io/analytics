8b167d28fa7db3bafda739325bc9299754d4485c
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-08-20 11:54:05 +0200

    fix: Implement new analytics sample item attribute component for profile list - MEED-2957 - Meeds-io/meeds#1301 (#209)

---
+++ analytics-webapps/src/main/webapp/vue-app/common-components/components/samples/ProfileListSampleItemAttribute.vue
@@ -0,0 +1,58 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+
+  Copyright (C) 2022 - 2024 Meeds Association contact@meeds.io
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <div>
+    <analytics-profile-chip
+      v-for="profileId in profileIds"
+      :key="profileId"
+      :profile-id="profileId"
+      class="pb-1"
+      display-value />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    attrKey: {
+      type: Object,
+      default: function () {
+        return null;
+      },
+    },
+    attrValue: {
+      type: Object,
+      default: function () {
+        return null;
+      },
+    },
+    options: {
+      type: Object,
+      default: function () {
+        return null;
+      },
+    },
+  },
+  computed: {
+    profileIds() {
+      return JSON.parse(this.attrValue);
+    },
+  },
+};
+</script>
