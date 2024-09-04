72f5c85d72b02743eee605dba5de595d29800a88
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-09-04 07:25:16 +0100

    feat: Portlet Analytics by Query - MEED-7414 - Meeds-io/MIPs#156 (#213)
    This change will introduce a new generic portlet
    (`AnalyticsQueryPortlet`) which will allow to display specific values
    retrieved from analytics queries in a portlet instance. A first usage of
    this generic portlet is the **Spaces List Widget** which will retrieve
    two type of data: '**Most Active Spaces**' and '**Recently Visited
    Spaces by Current User**'.
    The Analytics queries has to be defined in an init param to make it not
    modifiable through portlet preferences. To retrieve the data, the
    `serveResource` method URL of Portlet API (JSR 286) will be used by
    specifying the `queryName` in URL parameters which will execute the
    Analytics Filter and return the JSON result to be interpreted by Front
    End and finally displayed. The new field `hideLabel` has been added to
    the filter to allow not retrieving the space names to avoid Data Leak.

---
+++ analytics-webapps/src/main/webapp/vue-app/spaces-list-widget/main.js
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import './initComponents.js';
+
+const lang = eXo?.env?.portal?.language || 'en';
+const urls = [
+  `/analytics/i18n/locale.portlet.Analytics?lang=${lang}`,
+];
+const appId = 'spacesListWidget';
+
+export function init(
+  resourceURL,
+  portletStorageId,
+  spacesRecentlyVisitedLimit,
+  spacesMostActiveLimit,
+  canEdit,
+  pageRef,
+  canCreateSpace,
+  isExternal) {
+  exoi18n.loadLanguageAsync(lang, urls)
+    .then(i18n => {
+      Vue.createApp({
+        data: {
+          resourceURL,
+          portletStorageId,
+          spacesRecentlyVisitedLimit,
+          spacesMostActiveLimit,
+          canEdit: portletStorageId && canEdit || false,
+          pageRef,
+          canCreateSpace,
+          isExternal,
+        },
+        template: `<spaces-list-widget id="${appId}" />`,
+        i18n,
+        vuetify: Vue.prototype.vuetifyOptions,
+      }, `#${appId}`, 'Space List Widget');
+    });
+}
