983881ec3b7ea2e1cf344c3a70ac1d50d83b972a
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-25 09:25:29 +0100

    feat: Allow to edit JSON Settings Using UI - MEED-7571 - Meeds-io/meeds#2447 This change will change the readonly state of JSON panel and transform it from a dialog to drawer to let advanced users change it. In addition, the JSON panel option is added in Table Portlet as well.

---
+++ analytics-webapps/src/main/webapp/vue-app/common-components/components/settings/JsonPanelDrawer.vue
@@ -0,0 +1,98 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+
+  Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <exo-drawer
+    ref="chartSettingDrawer"
+    id="jsonChartSettingsModal"
+    v-model="drawer"
+    class="editChatSettings"
+    allow-expand
+    right>
+    <template #title>
+      {{ $t('analytics.jsonSettings.drawer.title') }}
+    </template>
+    <template #content>
+      <div class="pa-4 fill-height">
+        <textarea
+          v-model="settingJsonContent"
+          class="full-width full-height"
+          auto-grow />
+      </div>
+    </template>
+    <template #footer>
+      <div class="d-flex">
+        <v-spacer />
+        <v-btn
+          class="btn me-2"
+          elevation="0"
+          @click="drawer = false">
+          {{ $t('analytics.cancel') }}
+        </v-btn>
+        <v-btn
+          :disabled="!isValidJSON"
+          class="btn-primary"
+          color="primary"
+          elevation="0"
+          @click="save">
+          {{ $t('analytics.save') }}
+        </v-btn>
+      </div>
+    </template>
+  </exo-drawer>
+</template>
+<script>
+export default {
+  props: {
+    settings: {
+      type: Object,
+      default: null,
+    },
+  },
+  data: () => ({
+    drawer: false,
+    settingJsonContent: null,
+  }),
+  computed: {
+    settingsJSON() {
+      try {
+        return JSON.parse(this.settingJsonContent);
+      } catch(e) {
+        return null;
+      }
+    },
+    isValidJSON() {
+      return !!this.settingsJSON;
+    },
+  },
+  methods: {
+    open() {
+      this.drawer = true;
+      this.settingJsonContent = this.settings && JSON.stringify(this.settings, null, 2) || '{}';
+    },
+    save() {
+      if (!this.isValidJSON) {
+        return;
+      }
+      this.$emit('save', this.settingsJSON);
+      this.drawer = false;
+    },
+  },
+};
+</script>
