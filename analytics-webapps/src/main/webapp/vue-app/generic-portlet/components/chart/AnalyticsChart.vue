e6ca5466a02543a8a9e14b47a05293559c7c6a84
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2026-01-20 17:04:41 +0100

    fix: Analytics Field Data Types Detection - MEEDS-10142 - Meeds-io/meeds#3994 (#344)
    Prior to this change, when adding a StatisticData parameter, the field
    type is sometimes not well detected and rathen than using a Long it
    still uses a String. This change ensures to align Field Mapping Creation
    Request with Field Check Request. In addition, this change will allow to
    distinguish the new fields from the old fields by a suffix (New).

--- analytics-webapps/src/main/webapp/vue-app/generic-portlet/components/chart/AnalyticsChart.vue
+++ analytics-webapps/src/main/webapp/vue-app/generic-portlet/components/chart/AnalyticsChart.vue
@@ -235,8 +235,16 @@ export default {
         if (extension) {
           return extension.getLabel(fieldName, fieldValue);
         } else {
-          const fieldLabelI18NKey = `analytics.${fieldValue}`;
-          return this.$te(fieldLabelI18NKey) ? this.$t(fieldLabelI18NKey) : label;
+          let fieldLabelI18NKey = `analytics.${fieldValue?.replace?.('_alt', '')}`;
+          if (this.$te(fieldLabelI18NKey)) {
+            return fieldValue?.includes?.('_alt') ? this.$t('analytics.field.alternative', {0: this.$t(fieldLabelI18NKey)}) : this.$t(fieldLabelI18NKey);
+          } else {
+            fieldLabelI18NKey = `analytics.field.label.${fieldValue?.replace?.('_alt', '')}`;
+            return this.$te(fieldLabelI18NKey) ?
+              (fieldValue?.includes?.('_alt') ? this.$t('analytics.field.alternative', {0: this.$t(fieldLabelI18NKey)}) : this.$t(fieldLabelI18NKey)) // NOSONAR
+              : label;
+          }
+          
         }
       } else {
         return label;
