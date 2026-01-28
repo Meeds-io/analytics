c0d86b0758965dd34f532d5bd81b0e0317e0229b
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2026-01-28 11:29:10 +0100

    fix: Suggest users to add a space from the spaces list drawer - MEED-10162 - Meeds-io/meeds#3998 (#353)
    This change will add a CTA to allow users to add a space from the spaces list drawer.
    
    Resolves: Meeds-io/meeds#3998

--- analytics-webapps/src/main/webapp/vue-app/spaces-list-widget/components/SpacesListWidget.vue
+++ analytics-webapps/src/main/webapp/vue-app/spaces-list-widget/components/SpacesListWidget.vue
@@ -78,6 +78,7 @@
                 v-if="$root.canCreateSpace"
                 :color="'primary'"
                 :elevation="0"
+                :display-icon="false"
                 outlined
                 require-form-drawer
                 display-label />
